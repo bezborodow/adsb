31dc0e4bf031de8036303040b67100a738199ee8
Damien Bezborodov <dbezborodov@gmail.com>
2025-09-05 14:37:30 +0930

    61.44 sample positions.

--- research/sample_rate_calcs.py
+++ research/sample_rate_calcs.py
@@ -2,7 +2,7 @@
 
 # Sample rate in hertz or MSPS.
 fs = 61.44e6
-fs = 20e6
+#fs = 20e6
 print(f"fs = {fs} Hz")
 
 # Symbol length period in seconds.
@@ -17,3 +17,9 @@ print(f"samples_per_symbol = {samples_per_symbol}")
 n_preamble_symbols = 16 # Number of symbols in the preamble window.
 n_premble_buffer = 16 * samples_per_symbol
 print(f"n_premble_buffer = {n_premble_buffer}")
+
+# Preamble windows by sample index.
+preamble_symbol_positions = [0, 2, 7, 9]
+preamble_sample_positions = [x * samples_per_symbol for x in preamble_symbol_positions]
+print("preamble_sample_positions:")
+print(preamble_sample_positions)
