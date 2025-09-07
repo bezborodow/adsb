6a9bbfbfb7486973954f1a4c2e845c5e292f0d00
Damien Bezborodov <dbezborodov@gmail.com>
2025-09-08 01:04:57 +0930

    Work in progress on encoder.

--- ip/uart/uart_tx_enc.vhd
+++ ip/uart/uart_tx_enc.vhd
@@ -14,66 +14,128 @@ use work.uart_pkg.all;
 -- to a serial teletype (tty) terminal with each message terminated
 -- by newlines.
 entity uart_tx_enc is
-    generic (
-        DATA_WIDTH : positive := 8
-    );
     port (
         clk : in std_logic;
 
         -- Master.
         m_vld_i : in std_logic;
         m_rdy_o : out std_logic;
-        m_data_i : in std_logic_vector(DATA_WIDTH-1 downto 0);
+        m_data_i : in std_logic_vector(7 downto 0);
         m_ascii_i : in std_logic; -- Convert to ASCII.
         m_eom_i : in std_logic; -- End of message (EOM.)
 
         -- UART side (slave.)
         s_vld_o : out std_logic;
         s_rdy_i : in std_logic;
-        s_data_o : out std_logic_vector(DATA_WIDTH-1 downto 0)
+        s_data_o : out std_logic_vector(7 downto 0)
     );
 end uart_tx_enc;
 
 architecture rtl of uart_tx_enc is
     constant MAX_FRAMES : positive := 3; -- Maximum number of frames is two ASCII octets terminated with a newline.
+    constant ASCII_NEWLINE : std_logic_vector(7 downto 0) := X"0A";
 
-    -- Internal registers.
-    signal m_vld_r : std_logic := '0';
-    signal m_rdy_r : std_logic := '1';
-    signal m_data_r : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
-    signal m_ascii_r : std_logic := '0';
-    signal m_eom_r : std_logic := '0';
-    signal s_vld_r : std_logic := '0';
-    signal s_rdy_r : std_logic := '0';
-    signal s_data_r : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+    -- Combinatorial internal signals.
+    signal m_vld_c : std_logic := '0';
+    signal m_rdy_c : std_logic := '1';
+    signal m_data_c : std_logic_vector(7 downto 0) := (others => '0');
+    signal m_ascii_c : std_logic := '0';
+    signal m_eom_c : std_logic := '0';
+    signal s_vld_c : std_logic := '0';
+    signal s_rdy_c : std_logic := '0';
+    signal s_data_c : std_logic_vector(7 downto 0) := (others => '0');
 
     -- Buffer signals.
-    type frame_buffer_t is array (natural range <>) of std_logic_vector(DATA_WIDTH-1 downto 0);
-    signal frame_buffer : frame_buffer_t(0 to MAX_FRAMES-1) := (others => (others => '0'));
+    type frame_buffer_t is array (natural range <>) of std_logic_vector(7 downto 0);
+    signal encoder_buffer : frame_buffer_t(0 to MAX_FRAMES-1) := (others => (others => '0'));
+    signal sender_buffer : frame_buffer_t(0 to MAX_FRAMES-1) := (others => (others => '0'));
+    signal buffer_valid : std_logic := '0';
     signal buffer_ready : std_logic := '1';
 
 begin
-    -- Internal registers.
-    m_vld_r <= m_vld_i;
-    m_rdy_o <= m_rdy_r;
-    m_data_r <= m_data_i;
-    m_ascii_r <= m_ascii_i;
-    m_eom_r <= m_eom_i;
-    s_vld_o <= s_vld_r;
-    s_rdy_r <= s_rdy_i;
-    s_data_o <= s_data_r;
+    -- Internal staging/combinatorial signals.
+    m_vld_c <= m_vld_i;
+    m_rdy_o <= m_rdy_c;
+    m_data_c <= m_data_i;
+    m_ascii_c <= m_ascii_i;
+    m_eom_c <= m_eom_i;
+    s_vld_o <= s_vld_c;
+    s_rdy_c <= s_rdy_i;
+    s_data_o <= s_data_c;
 
-    buffer_encoder_process : process(clk)
+    s_data_c <= sender_buffer(0);
+
+    -- The encoder converts data into ASCII and stores it in the frame buffer.
+    encoder_process : process(clk)
     begin
         if rising_edge(clk) then
-            if buffer_ready = '1' then
+            if buffer_valid = '1' and buffer_ready = '1' then
+                buffer_valid <= '0';
+                m_rdy_c <= '1';
+            end if;
+
+            if m_vld_c = '1' and m_rdy_c = '1' then
+                -- Accept new data.
+                if m_ascii_c = '1' then
+                    -- ASCII mode.
+                    encoder_buffer(0) <= uart_ascii_hex(m_data_c(7 downto 4));
+                    encoder_buffer(1) <= uart_ascii_hex(m_data_c(3 downto 0));
+                    if m_eom_c = '1' then
+                        encoder_buffer(2) <= ASCII_NEWLINE;
+                    else
+                        encoder_buffer(2) <= (others => '0');
+                    end if;
+                else
+                    -- Binary mode.
+                    encoder_buffer(0) <= m_data_c;
+                    if m_eom_c = '1' then
+                        encoder_buffer(1) <= ASCII_NEWLINE;
+                    else
+                        encoder_buffer(1) <= (others => '0');
+                    end if;
+                    encoder_buffer(2) <= (others => '0');
+                end if;
+                buffer_valid <= '1';
+
+                -- If the sender is busy, then the buffer contents will not be accepted,
+                -- so need to delay the pipeline.
+                if buffer_ready = '0' then
+                    m_rdy_c <= '0';
+                end if;
             end if;
         end if;
-    end process buffer_encoder_process;
+    end process encoder_process;
 
+    -- The sender reads from the buffer and passes it to the UART transmitter slave.
     sender_process : process(clk)
+        variable buffer_available : boolean := false;
     begin
         if rising_edge(clk) then
+            if s_vld_c = '1' and s_rdy_c = '1' then
+                if sender_buffer(1) = x"00" then
+                    s_vld_c <= '0';
+                    buffer_ready <= '1';
+                else
+                    sender_buffer(0) <= sender_buffer(1);
+                    sender_buffer(1) <= sender_buffer(2);
+                    sender_buffer(2) <= x"00";
+                end if;
+            end if;
+
+            -- Accept data from the encoder buffer.
+            if buffer_valid = '1' and buffer_ready = '1' then
+                sender_buffer <= encoder_buffer;
+                s_vld_c <= '1';
+
+                -- The buffer will be available on the next cycle if the slave is
+                -- ready and the buffer will be empty afterwards. Or if the buffer
+                -- is already empty.
+                if (s_rdy_c = '1' and sender_buffer(1) = x"00") then
+                    buffer_ready <= '1';
+                else
+                    buffer_ready <= '0';
+                end if;
+            end if;
         end if;
     end process sender_process;
 end rtl;
--- tb/uart_tx_enc/uart_tx_enc_tb.vhd
+++ tb/uart_tx_enc/uart_tx_enc_tb.vhd
@@ -13,27 +13,28 @@ end uart_tx_enc_tb;
 
 architecture test of uart_tx_enc_tb is
     signal clk : std_logic := '1';
-    constant clk_period : time := 16 ns;
-
-    constant ENC_DATA_WIDTH : integer := 8;
+    constant clk_period : time := 20 ns;
 
     signal master_vld : std_logic := '0';
     signal master_rdy : std_logic := '0';
-    signal master_data : std_logic_vector(ENC_DATA_WIDTH-1 downto 0) := (others => '0');
+    signal master_data : std_logic_vector(7 downto 0) := (others => '0');
     signal master_ascii : std_logic := '0';
     signal master_eom : std_logic := '0';
 
     signal slave_vld : std_logic := '0';
     signal slave_rdy : std_logic := '0';
-    signal slave_data : std_logic_vector(ENC_DATA_WIDTH-1 downto 0) := (others => '0');
+    signal slave_data : std_logic_vector(7 downto 0) := (others => '0');
 
+    procedure wait_for_clock_cycles(signal clock : std_logic; n : natural) is
+    begin
+      for i in 1 to n loop
+        wait until rising_edge(clock);
+      end loop;
+    end procedure;
 begin
     clk <= not clk after clk_period / 2;
 
     uut : entity work.uart_tx_enc
-        generic map (
-            DATA_WIDTH => ENC_DATA_WIDTH
-        )
         port map (
             clk => clk,
             m_vld_i => master_vld,
@@ -50,10 +51,62 @@ begin
     begin
         test_runner_setup(runner, runner_cfg);
 
+        -- Idle.
+        wait_for_clock_cycles(clk, 10);
+
+        -- Put some data out on the line.
+        assert master_rdy = '1' report "Master should be ready." severity failure;
+        assert slave_vld = '0' report "No data should be valid for the slave yet." severity failure;
+        assert slave_rdy = '0' report "Slave should be busy." severity failure;
+
+        master_data <= x"4D";
+        master_ascii <= '0';
+        master_eom <= '0';
+        master_vld <= '1';
+        wait for clk_period;
+        assert master_rdy = '1' report "Master should be ready." severity failure;
+        master_vld <= '0';
+        wait for clk_period * 3;
+
+        -- Idle for a while with the slave being busy.
+        wait for clk_period * 10;
+        assert master_rdy = '1' report "Master should be ready." severity failure;
+
+        -- Choke up the encoder and sender buffer.
+        master_data <= x"4E";
+        master_vld <= '1';
+        wait until rising_edge(clk);
+
+        master_vld <= '0';
+        wait until rising_edge(clk);
+        --assert master_rdy = '0' report "Master should not be ready." severity failure;
+
+        -- Idle for a while longer with the slave still being busy.
+        wait for clk_period * 10;
+
+        -- Free up the slave to allow data to flow.
+        slave_rdy <= '1';
         wait for clk_period;
 
-        --assert master_rdy = '1' report "Master should be ready." severity failure;
+        master_vld <= '0';
+        wait for clk_period * 10;
+
+        -- Send three characters consecutively (XYZ) without any back pressure
+        -- from the slave.
+        slave_rdy <= '1';
+        master_vld <= '1';
+        master_data <= x"58";
+        master_ascii <= '0';
         wait for clk_period;
+        master_data <= x"59";
+        wait for clk_period;
+        master_data <= x"5A";
+        wait for clk_period;
+
+        master_vld <= '0';
+        wait for clk_period * 10;
+
+        -- Send some numbers (converted to ASCII with newlines.)
 
         test_runner_cleanup(runner); -- Simulation ends here
         wait;
