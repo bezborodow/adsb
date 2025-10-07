cdbe4eda97b91803c40a347d1158cb06b50401ac
Damien Bezborodov <dbezborodov@gmail.com>
2025-10-08 00:15:14 +1030

    Do not neee circular buffer inside rolling sum.
    (The tapped buffer does that.)

--- ip/adsb/adsb_rolling_sum.vhd
+++ ip/adsb/adsb_rolling_sum.vhd
@@ -19,15 +19,8 @@ entity adsb_rolling_sum is
 end adsb_rolling_sum;
 
 architecture rtl of adsb_rolling_sum is
-    -- Constrain the buffer to ROLLING_BUFFER_LENGTH
-    signal circular_buf : mag_sq_buffer_t(0 to ROLLING_BUFFER_LENGTH-1) := (others => (others => '0'));
-
-    -- Pointer for circular buffer.
-    signal head_ptr : integer range 0 to ROLLING_BUFFER_LENGTH-1 := 0;
-
     -- Sum output register.
     signal sum_r : unsigned(SUM_WIDTH-1 downto 0) := (others => '0');
-
 begin
 
     -- Drive output registers.
@@ -40,13 +33,6 @@ begin
     begin
         if rising_edge(clk) then
             if ce_i = '1' then
-
-                -- Insert incoming sample into the buffer.
-                -- (Outgoing sample gets overwritten.)
-                circular_buf(head_ptr) <= incoming_i;
-
-                -- Advance circular pointer.
-                head_ptr <= (head_ptr + 1) mod ROLLING_BUFFER_LENGTH;
 
                 -- Compute rolling sum.
                 incoming_v := resize(incoming_i, SUM_WIDTH);
